91a75c6ef7a6539184f8743f066ebfbb26fa10a1
Anthony Doupe <atlamillias@gmail.com>
2021-06-25 17:46:04 -0700

    added "reset_pos" to Container class

--- smartwidgets/bases.py
+++ smartwidgets/bases.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from typing import Callable
 
-from dearpygui import core
+from dearpygui import core as dpg
 
 
 # needs handler & theme support
@@ -17,30 +17,33 @@ class Widget(metaclass=ABCMeta):
         # is much better
         if parent := kwargs.pop("parent", None):
             if isinstance(parent, Container):
-                core.push_container_stack(parent)
+                dpg.push_container_stack(parent)
             else:
                 kwargs["parent"] = parent.id
                 parent = None
 
-        self.__id = self._command(**kwargs)
+        self.__id = self._command(**kwargs) 
 
         if parent:
-            core.pop_container_stack()
+            dpg.pop_container_stack()
 
         [self.__config.add(optn) for optn in kwargs.keys()]
 
     def __str__(self):
         return f"{self.__id}"
 
+    def __int__(self):
+        return self.__id
+
     def __repr__(self):
-        mVAppType = core.get_item_info(self.id)["type"].lstrip('::')[-1]
+        mVAppType = dpg.get_item_info(self.id)["type"].lstrip('::')[-1]
         return f"{self.__class__.__qualname__} ({type(self)}) => \
             (<{mVAppType} '{self.__id}'>)"
 
     def __getattr__(self, attr):
         if attr in self.__config:
             try:
-                return core.get_item_configuration(self.__id)[attr]
+                return dpg.get_item_configuration(self.__id)[attr]
             except KeyError:
                 return super().__getattr__(attr)
 
@@ -53,7 +56,7 @@ class Widget(metaclass=ABCMeta):
         # be changed, not set.
         # i.e. __iadd__, .append for a list value
         if attr in self.__config:
-            core.configure_item(self.__id, **{attr: value})
+            dpg.configure_item(self.__id, **{attr: value})
         else:
             # might have to un-nest (above comment)
             super().__setattr__(attr, value)
@@ -65,42 +68,42 @@ class Widget(metaclass=ABCMeta):
     @property
     def state(self):
         """Return the current state of the widget."""
-        return core.get_item_state(self.__id)
+        return dpg.get_item_state(self.__id)
 
     @property
     def value(self):
         """Return the widget value (if any)."""
-        return core.get_value(self.__id)
+        return dpg.get_value(self.__id)
 
     def move_up(self):
-        core.move_item_up(self.__id)
+        dpg.move_item_up(self.__id)
 
     def move_down(self):
-        core.move_item_down(self.__id)
+        dpg.move_item_down(self.__id)
 
     def move(self, parent: int, before: int):
         """Move a widget to another <parent> before <before>."""
-        core.move_item(self.__id, parent, before)
+        dpg.move_item(self.__id, parent, before)
 
     def refresh(self):
         """Deletes all children in the widget, if any."""
-        core.delete_item(self.__id, children_only=True)
+        dpg.delete_item(self.__id, children_only=True)
 
     def remove(self):
         """Deletes the widget (and children)."""
-        core.delete_item(self.__id)
+        dpg.delete_item(self.__id)
 
     def configure(self, **config):
         """Updates the widget configuration."""
-        core.configure_item(self.__id, **config)
+        dpg.configure_item(self.__id, **config)
 
     def configuration(self, option=None):
         """Returns the entire current widget configuration, or
         only <option> (if specified)."""
         if option:
-            return core.get_item_configuration(self.__id)[option]
+            return dpg.get_item_configuration(self.__id)[option]
 
-        return core.get_item_configuration(self.__id)
+        return dpg.get_item_configuration(self.__id)
 
 
 class Container(Widget, metaclass=ABCMeta):
@@ -108,8 +111,11 @@ class Container(Widget, metaclass=ABCMeta):
     def _command() -> Callable: ...
 
     def __enter__(self):
-        core.push_container_stack(self.id)
+        dpg.push_container_stack(self.id)
         return self
 
     def __exit__(self, exec_type, exec_value, traceback):
-        core.pop_container_stack(self.id)
+        dpg.pop_container_stack(self.id)
+
+    def reset_pos(self):
+        dpg.reset_pos(self.__id)
