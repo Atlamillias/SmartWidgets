1fe5e1d551b016704dae0460cd5ca5c720773379
Anthony Doupe <atlamillias@gmail.com>
2021-06-25 17:46:33 -0700

    cleanup/refactor

--- smartwidgets/registries.py
+++ smartwidgets/registries.py
@@ -1,55 +1,51 @@
-from typing import (
-    Callable,
-)
+from typing import Callable
+from abc import abstractmethod, ABCMeta
 
-from dearpygui import core
+from dearpygui import core as dpg
 
 from .bases import Container
 
 
-class FontRegistry(Container):
-    _command: Callable = core.add_font_registry
+class RegistryPropertiesMixin:
+    __fonts = None
+    __handlers = None
+    __textures = None
+    __values = None
+
+
+
+
+class Registry(Container, metaclass=ABCMeta):
+    @abstractmethod
+    def _command() -> Callable: ...
 
     def __init__(
         self,
         show: bool = True,
         label: str = None,
+        **kwargs,
     ):
         super().__init__(
             show=show,
             label=label,
+            **kwargs
         )
         self.show = show
         self.label = label
 
 
-class HandlerRegistry(Container):
-    _command: Callable = core.add_handler_registry
+class FontRegistry(Registry):
+    _command = dpg.add_font_registry
 
-    def __init__(
-        self,
-        show: bool = True,
-        label: str = None,
-    ):
-        super().__init__(
-            show=show,
-            label=label,
-        )
-        self.show = show
-        self.label = label
+class HandlerRegistry(Registry):
+    _command = dpg.add_handler_registry
 
+class TextureRegistry(Registry):
+    _command = dpg.add_texture_registry
 
-class TextureRegistry(Container):
-    _command: Callable = core.add_texture_registry
+class ValueRegistry(Container):  # excludes "show"/"enabled"
+    _command = dpg.add_value_registry
 
-    def __init__(
-        self,
-        show: bool = True,
-        label: str = None,
-    ):
-        super().__init__(
-            show=show,
-            label=label,
-        )
-        self.show = show
+    def __init__(self, label: str = None, **kwargs):
+        super().__init__(label=label, **kwargs)
         self.label = label
