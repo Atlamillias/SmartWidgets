11043c19aeb7bbc780836eb7a74bfe8ef8b8e57b
Anthony Doupe <atlamillias@gmail.com>
2021-07-22 13:50:55 -0700

    Minot optimizations, state no longer a property.

--- _item.py
+++ _item.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 from typing import Callable
 
 from . import dpg
+from dearpygui import _dearpygui as idpg
 
 
 class Item(metaclass=ABCMeta):
@@ -11,17 +12,25 @@ class Item(metaclass=ABCMeta):
     __config = set()
 
     def __init__(self, **kwargs):
-        super().__init__()
         if parent := kwargs.pop("parent", None):
             kwargs["parent"] = int(parent)
-
         kwargs["label"] = kwargs.get('label') or self.__class__.__name__
+
         self.__id = self.__class__._command(**kwargs)
 
-        self.__config = {optn for optn in kwargs.keys()}
+        # cache config attrs for future items of the same type
+        if not self.__config:
+            self.__class__.__config = {optn for optn in kwargs.keys()
+                                       if optn != "id" and
+                                       optn != "default_value"}
+
+        super().__init__()
 
     def __str__(self):
-        return f"{self.__id}"
+        try:
+            return self.label
+        except AttributeError:
+            return f"{self.__id}"
 
     def __int__(self):
         return self.__id
@@ -31,54 +40,59 @@ class Item(metaclass=ABCMeta):
         return f"{self.__class__.__qualname__} ({type(self)}) => \
             (<{mVAppType} '{self.__id}'>)"
 
-    def __getattr__(self, attr):
-        if attr in self.__config:
-            try:
-                return dpg.get_item_configuration(self.__id)[attr]
-            except KeyError:
-                pass
-
-        return super().__getattr__(attr)
+    def __getattribute__(self, attr: str):
+        if attr.startswith("_") or attr == "id":
+            return super().__getattribute__(attr)
+        elif attr in super().__getattribute__("_Item__config"):
+            id = super().__getattribute__("_Item__id")
+            return dpg.get_item_configuration(id)[attr]
+        return super().__getattribute__(attr)
 
     def __setattr__(self, attr, value):
-        if attr in self.__config:
+        if attr in object.__getattribute__(self, "_Item__config"):
             dpg.configure_item(self.__id, **{attr: value})
         else:
-            super().__setattr__(attr, value)
+            object.__setattr__(self, attr, value)
 
     @property
     def id(self):
         return self.__id
 
     @property
-    def state(self):
-        """Return the current state of the widget."""
-        return dpg.get_item_state(self.__id)
-
-    @property
     def value(self):
         """Return the widget value (if any)."""
-        return dpg.get_value(self.__id)
+        return idpg.get_value(self.__id)
+
+    @value.setter
+    def value(self, value):
+        idpg.set_value(self.__id, value)
+
+    def set_value(self, value):
+        idpg.set_value(self.__id, value)
 
     def remove(self):
         """Deletes the widget (and children)."""
-        dpg.delete_item(self.__id)
+        idpg.delete_item(self.__id)
 
     def refresh(self):
         """Deletes all children in the widget, if any."""
-        dpg.delete_item(self.id, children_only=True)
+        idpg.delete_item(self.id, children_only=True)
 
     def configure(self, **config):
         """Updates the widget configuration."""
-        dpg.configure_item(self.__id, **config)
+        idpg.configure_item(self.__id, **config)
 
     def configuration(self, option=None):
         """Returns the entire current widget configuration, or
         only <option> if specified."""
         if option:
-            return dpg.get_item_configuration(self.__id)[option]
+            return idpg.get_item_configuration(self.__id)[option]
 
-        return dpg.get_item_configuration(self.__id)
+        return idpg.get_item_configuration(self.__id)
+
+    def state(self) -> dict:
+        """Return the current state of the widget."""
+        return idpg.get_item_state(self.__id)
 
 
 class ContextSupport:  # mixin
